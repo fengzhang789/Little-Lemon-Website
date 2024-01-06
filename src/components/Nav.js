b7f945c04752950d5f77f17aaf1b290c7d5eae30
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-01-05 22:25:28 -0500

    added metadata for SEO

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,22 @@ const Nav = () => {
         <nav>
             <ul>
                 <li>
-                    <a></a>
+                    <a>Home</a>
+                </li>
+                <li>
+                    <a>About</a>
+                </li>
+                <li>
+                    <a>Menu</a>
+                </li>
+                <li>
+                    <a>Reservations</a>
+                </li>
+                <li>
+                    <a>Order Online</a>
+                </li>
+                <li>
+                    <a>Login</a>
                 </li>
             </ul>
         </nav>
